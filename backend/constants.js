8ab7ab46ef9e5db1334c00d4d680b9e1e7db26ef
Dos Santos <isadora.m.dos.santos@accenture.com>
2023-02-07 09:59:54 +0100

    Initial Commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x1F175379E58d66eB721F39d41b457A7C52475400";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
